7ef6448bcecd98814ba5680bba2da2296750eced
Gift-py <giftabumere247@gmail.com>
2024-02-07 21:13:16 +0100

    Done for the day lmao

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,12 +14,8 @@ import clsx from 'clsx';
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/customers', icon: UserGroupIcon },
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon, },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
